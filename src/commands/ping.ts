be0e0c961de164ed8781d25b98a89d5fbcb68506
ShuMac000 <syuuzired@gmail.com>
2022-06-20 20:09:06 +0900

    first commit

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -13,7 +13,7 @@ export function ping(interaction:ApplicationCommandInteraction):InteractionRespo
       // input at the top.
       type: InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
-        content: `pong!`,
+        content: `ping///`,
       }
     }
   return res;
